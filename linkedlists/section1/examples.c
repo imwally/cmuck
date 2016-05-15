7dc0a099bc896bf88db7ec4dbebe51f83a3daa8e
Wally <wally@imwally.net>
2016-05-15 19:58:07 -0400

    add length function

--- linkedlists/section1/examples.c
+++ linkedlists/section1/examples.c
@@ -32,6 +32,22 @@ struct node* BuildOneTwoThree() {
     return head;
 }
 
+/*
+* Given a linked list head pointer, compute
+* and return the number of nodes in the list.
+*/
+int Length(struct node* head) {
+   struct node* current = head;
+
+   int count = 0;
+   while (current != NULL) {
+      count++;
+      current = current->next;
+   }
+
+   return count;
+}
+
 int main() {
     struct node* head = BuildOneTwoThree();
 
@@ -40,4 +56,8 @@ int main() {
         printf("%d\n", current->data);
         current = current->next;
     }
+
+    int len;
+    len = Length(head);
+    printf("length: %d\n", len);
 }
